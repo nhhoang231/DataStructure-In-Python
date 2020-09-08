a1aee106329052dd99928da22bb72eeb2283cd5f
Nguyễn Huy Hoàng <63738708+nhhoang231@users.noreply.github.com>
2020-09-08 15:24:05 +0700

    Create InsertionInLinkedList.py

---
+++ LinkedList/InsertionInLinkedList.py
@@ -0,0 +1,54 @@
+
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+        
+# Insertion at the beginning of a linked list
+# Print the linked list
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+    def AtBegining(self,newdata):
+        NewNode = Node(newdata)
+
+# Update the new nodes next val to existing node
+        NewNode.nextval = self.headval
+        self.headval = NewNode
+        
+# Insertion at the end of a linked list
+# Function to add newnode
+    def AtEnd(self, newdata):
+        NewNode = Node(newdata)
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        laste = self.headval
+        while(laste.nextval):
+            laste = laste.nextval
+        laste.nextval=NewNode
+
+# Print the linked list
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtBegining("Sun")
+l
+list.listprint()
